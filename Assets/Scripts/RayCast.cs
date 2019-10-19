8d218568027fec831535caf35b67492a8c3ade8c
Nikita Vispute <nikitavisp@gmail.com>
2019-10-19 16:12:43 -0500

    First commit

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,393 @@
+﻿/*
+ * 
+ * Teleportation script for 5UDE VR simulator.
+ * Author: Yash Mewada
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class RayCast : MonoBehaviour {
+
+
+    // Inspector parameters
+    [Tooltip("The tracking device used to determine absolute direction for steering.")]
+    public CommonTracker tracker;
+
+ 
+    [Tooltip("A button required to be pressed to activate steering.")]
+    public CommonButton raycastButton;
+
+    
+
+
+    [Tooltip("The space that is translated by this interaction. Usually set to the physical tracking space.")]
+    public CommonSpace space;
+
+
+    //[Header("Bounds for raycasting")]
+    ////LineRenderer Prefab Useful for drawing outline 
+    //public GameObject lineRendererPrefab;
+    //private GameObject lineGenerator;
+
+    //[Header("Raycast Boundary indicator")]
+    //public GameObject raycastBoundary;
+    //private ParticleSystem raycastParticle;
+
+    [Header("Laser prefab and teleport reticle prefab")]
+    //creates a laser for teleporting
+    public GameObject laserPrefab;
+    public GameObject raycastReticlePrefab;
+
+    //enable invalid raycast
+    public GameObject invalidLaserPrefab;
+    private GameObject invalidLaser;
+    private Transform invalidLaserTransform;
+    
+
+
+    //creating the reticle for target to teleport to.
+    [Tooltip("The target marker for the teleporting script.")]
+
+    private GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+    private GameObject reticle;
+    private Transform raycastReticleTransform;
+    public Vector3 raycastReticleOffset;
+    public LayerMask rayCastMask;
+
+
+    [Header("Bounds for validity of raycast")]
+    //Radius bounds.
+    public float validRaycastRadius = 2.0f;
+    public LayerMask detectableObjectLayer;
+
+
+    [Header("CanvasUI Prefab to show the food info")]
+    //CanvasUIPrefab
+    //public GameObject canvasUIPrefab;
+    public GameObject canvasUI;
+    
+
+
+    [Header("Image Data for information about food")]
+    //Image for data
+    public Sprite canvasPigImage;
+    public Sprite canvasCornImage;
+    public Sprite canvasWheatImage;
+    public Sprite canvasChickenImage;
+    public Sprite canvasCowImage;
+
+    protected Image canvasUIimage;
+
+    [Header("Sounds for the animal information")]
+    public AudioClip pigInfoSound;
+    public AudioClip cowInfoSound;
+    public AudioClip wheatInfoSound;
+    public AudioClip cornInfoSound;
+    public AudioClip chickenInfoSound;
+    public bool isAudioPlaying = false;
+    private AudioSource infoAudioSource;
+
+   
+
+    // Use this for initialization
+    void Start () {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+        reticle = Instantiate(raycastReticlePrefab);
+        raycastReticleTransform = reticle.transform;
+        invalidLaser = Instantiate(invalidLaserPrefab);
+        invalidLaserTransform = invalidLaser.transform;
+        canvasUIimage = canvasUI.GetComponentInChildren<Image>();
+        //lineGenerator = Instantiate(lineRendererPrefab);
+        //raycastParticle = raycastBoundary.GetComponent<ParticleSystem>();
+        
+        
+
+        
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(tracker.transform.position, validRaycastRadius);
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        if (!raycastButton.GetPress())
+        {
+            //setRaycastBoundParams();
+            //raycastBoundary.SetActive(false);
+            laser.SetActive(false);
+            reticle.SetActive(false);
+            canvasUI.SetActive(false);
+            invalidLaser.SetActive(false);
+          
+        }
+
+        if(raycastButton.GetPress()){
+            //setRaycastBoundParams();
+            //raycastBoundary.SetActive(true);
+            RaycastHit hit;
+           
+            if (Physics.Raycast(tracker.transform.position, tracker.transform.forward, out hit, rayCastMask))
+            {
+                
+                hitPoint = hit.point;
+               
+                if (insideObjectRaycastBounds(hitPoint))
+                {
+                    showLaser(hit,true);
+                    raycastReticleTransform.position = hitPoint + raycastReticleOffset;
+                    GameObject hitObject = hit.transform.gameObject;
+                    Debug.Log(hitObject.name);
+                    showUIAndGenerateCanvas(hitObject);
+                }
+                else
+                {
+                    showLaser(hit, false);
+                    canvasUI.SetActive(false);
+                }
+              
+                
+                
+                
+
+
+            }
+        }
+
+      
+
+    }
+
+
+
+    //private void Update()
+    //{
+    //    //if (infoAudioSource != null)
+    //    //{
+    //    //    // Set the audiosource's loop setting
+    //    //    infoAudioSource.loop = false;
+    //    //    // Set the audiosource's volume
+    //    //    infoAudioSource.volume = 2f;
+    //    //    // Set the audiosource's minimum distance
+    //    //    infoAudioSource.minDistance = 5;
+    //    //    // Set the audiosource's maximum distance
+    //    //    infoAudioSource.maxDistance = 100;
+    //    //    // Set the audiosource's blend setting to 3D
+    //    //    infoAudioSource.spatialBlend = 1.0f;
+    //    //    Debug.Log("CLIP : " + infoAudioSource.clip.name);
+    //    //    infoAudioSource.Play();
+    //    //}
+    //}
+
+
+
+
+    //set raycast boundary particle system params;
+    //private void setRaycastBoundParams()
+    //{
+    //    Vector3 particlePosition = new Vector3(tracker.transform.position.x, 0, tracker.transform.position.z);
+    //    raycastBoundary.transform.position = particlePosition;
+    //    raycastParticle.transform.position = particlePosition;
+
+    //    return;
+
+    //}
+
+    private void showUIAndGenerateCanvas(GameObject hitObject)
+    {
+ 
+        if (hitObject.CompareTag("animal"))
+        {
+            //Debug.Log(hitObject.name);
+            string selectedObjectName = hitObject.name.ToLower();
+            Vector3 selectedObjectPosition = hitObject.transform.position;
+            string animalName = getObjectName(selectedObjectName);
+
+            //getting collider of animal and then setting the canvas ui above the height of collider
+            Collider animalCollider = hitObject.GetComponent<Collider>();
+
+           infoAudioSource = hitObject.GetComponent<AudioSource>();
+           
+            canvasUI.transform.eulerAngles = new Vector3(0, tracker.transform.eulerAngles.y, 0);
+            canvasUI.transform.position = new Vector3(selectedObjectPosition.x, animalCollider.bounds.size.y+0.8f, selectedObjectPosition.z);
+           
+           // Debug.Log("Canvas OBJECT LOCATION : " + canvasUI.transform.position);
+            canvasUIimage.transform.position = canvasUI.transform.position;
+            if (animalName != "")
+            {
+                Sprite infoAboutAnimal = getFoodSources(animalName);
+               
+                if (infoAboutAnimal!= null)
+                {
+
+                    setUIDetails(animalName, infoAboutAnimal);
+                  
+                    
+                }
+                //myText.color = Color.Lerp(myText.color, Color.white, 6f * Time.deltaTime);
+            }
+        }
+        else
+        {
+            //myText.color = Color.Lerp(myText.color, Color.clear, 5f * Time.deltaTime);
+            canvasUI.SetActive(false);
+        }
+    }
+
+    private AudioClip getAudioInfo(string animalName)
+    {
+        if (name == "pig")
+        {
+            return pigInfoSound;
+        }
+        else if (name == "cow")
+        {
+
+            Debug.Log("returning cow " );
+            return cowInfoSound;
+        }
+        else if (name == "chicken")
+        {
+            return chickenInfoSound;
+        }
+        else if (name == "wheat")
+        {
+            return wheatInfoSound;
+        }
+        else if (name == "corn")
+        {
+            return cornInfoSound;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private Sprite getFoodSources(string name){
+        if(name == "pig"){
+            return canvasPigImage;
+        }else if(name == "cow"){
+            return canvasCowImage;
+        }else if(name == "chicken"){
+            return canvasChickenImage;
+        }else if(name == "wheat"){
+            return canvasWheatImage;
+        }else if( name == "corn"){
+            return canvasCornImage;
+        }else{
+            return null;
+        }
+
+
+    }
+
+    //Bound for raycast
+   private bool insideObjectRaycastBounds(Vector3 hitPoint)
+    {
+       
+        Collider[] hitColliders = Physics.OverlapSphere(tracker.transform.position, validRaycastRadius,detectableObjectLayer);
+        //Debug.Log("Checking inside radius : " + (hitPoint - tracker.transform.position).sqrMagnitude);
+        
+        //Debug.Log("in sphere? :  " + ((hitPoint - tracker.transform.position).sqrMagnitude < validRaycastRadius * validRaycastRadius));
+        if((hitPoint - tracker.transform.position).sqrMagnitude < validRaycastRadius * validRaycastRadius)
+        {
+            
+            return true;
+        }
+        return false;
+
+    }
+
+    private void showLaser(RaycastHit hit, bool valid)
+    {
+
+        if (valid)
+        {
+            invalidLaser.SetActive(false);
+            reticle.SetActive(true);
+            //reticleInvalid.SetActive(false);
+            laser.SetActive(true);
+            laser.transform.position = Vector3.Lerp(tracker.transform.position, hitPoint, .5f);
+            laserTransform.LookAt(hitPoint);
+            laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
+        }
+        else
+        {
+            laser.SetActive(false);
+            reticle.SetActive(false);
+            invalidLaser.SetActive(true);
+            invalidLaser.transform.position = Vector3.Lerp(tracker.transform.position, hitPoint, .5f);
+            invalidLaserTransform.LookAt(hitPoint);
+            invalidLaserTransform.localScale = new Vector3(invalidLaserTransform.localScale.x, invalidLaserTransform.localScale.y, hit.distance);
+        }
+
+
+
+    }
+    private void setUIDetails(string name,  Sprite image){
+        
+        canvasUIimage.sprite = image;
+
+        canvasUI.SetActive(true);
+
+    }
+
+    private string getObjectName(string selectedObjectName){
+        string textResult = "";
+        if(selectedObjectName.Contains("pig")){
+            textResult = "pig";
+        }else if(selectedObjectName.Contains("chicken")){
+            textResult = "chicken";
+
+        }
+        else if(selectedObjectName.Contains("cow")){
+            textResult = "cow";
+
+        }
+        else if(selectedObjectName.Contains("wheat")){
+            textResult = "wheat";
+
+        }
+        else if(selectedObjectName.Contains("corn")){
+            textResult = "corn";
+
+        }
+
+
+        return textResult;
+    }
+
+
+    
+
+
+  
+    
+
+    
+
+
+
+
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,185 @@
+﻿/*
+ * 
+ * Teleportation script for 5UDE VR simulator.
+ * Author: Yash Mewada
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class Teleport : MonoBehaviour {
+
+   
+    // Inspector parameters
+    [Tooltip("The tracking device used to determine absolute direction for steering.")]
+    public CommonTracker tracker;
+
+   
+
+    [Tooltip("A button required to be pressed to activate steering.")]
+    public CommonButton teleportButton;
+
+
+    [Tooltip("The space that is translated by this interaction. Usually set to the physical tracking space.")]
+    public CommonSpace space;
+
+    [Tooltip("The median speed for movement expressed in meters per second.")]
+    public float speed = 1.0f;
+
+
+    ////Line Renderer for bezier Curve
+    //public GameObject teleportPointersPrefab;
+    //private GameObject linePointer;
+
+
+    //creates a laser for teleporting
+    public GameObject laserPrefab;
+    private GameObject laser;
+    public GameObject laserInvalidPrefab;
+    private GameObject laserInvalid;
+    private Transform laserInvalidTransform;
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+    private GameObject hitObject;
+
+
+    //creating the reticle for target to teleport to.
+    [Tooltip("The target marker for the teleporting script.")]
+    public GameObject teleportReticlePrefab;
+    public GameObject teleportReticleInvalidPrefab;
+
+    private GameObject reticleInvalid;
+    private Transform teleportReticleInvalidTransform;
+    private GameObject reticle;
+    private Transform teleportReticleTransform;
+    public Vector3 teleportReticleOffset;
+    public LayerMask teleportMask;
+    public bool shouldTeleport;
+
+
+
+    // Use this for initialization
+    void Start () {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+        laserInvalid = Instantiate(laserInvalidPrefab);
+        laserInvalidTransform = laserInvalid.transform;
+        reticle = Instantiate(teleportReticlePrefab);
+        teleportReticleTransform = reticle.transform;
+        reticleInvalid = Instantiate(teleportReticleInvalidPrefab);
+        teleportReticleInvalidTransform = reticleInvalid.transform;
+       
+        // myText = GameObject.Find("Text").GetComponent<Text>();
+        // myText.color = Color.clear;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (!teleportButton.GetPress())
+        {
+            laser.SetActive(false);
+            laserInvalid.SetActive(false);
+            reticle.SetActive(false);
+            reticleInvalid.SetActive(false);
+           
+
+        }
+
+        if(teleportButton.GetPress()){
+            RaycastHit hit;
+          
+            if (Physics.Raycast(tracker.transform.position, tracker.transform.forward, out hit, teleportMask))
+            {
+                hitPoint = hit.point;
+                hitObject = hit.transform.gameObject;
+
+
+                if (hitObject.tag.Contains("canTeleport"))
+                {
+                    showLaser(hit, true);
+                    reticle.SetActive(true);
+                    teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                    shouldTeleport = true;
+                }
+                else
+                {
+                    showLaser(hit, false);
+                    reticleInvalid.SetActive(true);
+                    shouldTeleport = false;
+                    reticle.SetActive(false);
+                    teleportReticleInvalidTransform.position = hitPoint + teleportReticleOffset;
+                }
+            }
+        }
+
+        if (shouldTeleport && !teleportButton.GetPress())
+        {
+            laserInvalid.SetActive(false);
+            TeleportToTarget();
+        }
+
+
+
+
+
+    }
+
+    private void showLaser(RaycastHit hit,bool valid){
+
+        if (valid)
+        {
+            laserInvalid.SetActive(false);
+            reticleInvalid.SetActive(false);
+            laser.SetActive(true);
+            laser.transform.position = Vector3.Lerp(tracker.transform.position, hitPoint, .5f);
+            laserTransform.LookAt(hitPoint);
+            laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
+        }
+        else
+        {
+            laser.SetActive(false);
+            reticleInvalid.SetActive(true);
+            laserInvalid.SetActive(true);
+            laserInvalid.transform.position = Vector3.Lerp(tracker.transform.position, hitPoint, .5f);
+            laserInvalidTransform.LookAt(hitPoint);
+            laserInvalidTransform.localScale = new Vector3(laserInvalidTransform.localScale.x, laserInvalidTransform.localScale.y, hit.distance);
+        }
+
+            
+
+    }
+
+    public void TeleportToTarget(){
+
+        shouldTeleport = false;
+
+        reticle.SetActive(false);
+
+      
+        space.transform.position = hitPoint;
+
+    }
+
+
+
+    //calculating one point for bezier curve.
+    public Vector3 calculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+
+
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+
+        Vector3 p = (uu * p0) + (2 * u * t * p1) + (tt * p2);
+        return p;
+    }
+
+
+}
